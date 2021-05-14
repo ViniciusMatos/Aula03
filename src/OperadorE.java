8261f8f297d878ca24109dd1f1b6420f65438448
Vinicius Matos <vmatos12@gmail.com>
2021-05-14 20:16:03 -0300

    Implementado o operador E

--- src/OperadorE.java
+++ src/OperadorE.java
@@ -13,8 +13,11 @@ public class OperadorE {
 		senha = leitor.next();	
 		//Aqui é preciso criar um if que verifique as duas condições para o login ser bem sucedido
 		
-		
-		
+		if(usuario.equals("administrador") && senha.equals("123")) {
+			System.out.println("Usuário e senha corretos, bem vindo!");
+		}else {
+			System.out.println("Usuário ou senha incorretos");
+		}
 		leitor.close();
 
 	}
